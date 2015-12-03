5cfbf64a1e20ff0b1663745d84a9833912ef5a57
S. Grimault <sebastien.grimault@makina-corpus.com>
2015-12-03 10:13:38 +0100

    SyncCommandIntentService update: * lint warnings * 'INTENT_DELETE_INPUT' and 'INTENT_MOVE_FILE_TO_EXTERNAL_STORAGE' commands are now deprecated

--- commons/src/main/java/com/makina/ecrins/commons/sync/SyncCommandIntentService.java
+++ commons/src/main/java/com/makina/ecrins/commons/sync/SyncCommandIntentService.java
@@ -2,15 +2,16 @@ package com.makina.ecrins.commons.sync;
 
 import android.annotation.SuppressLint;
 import android.app.IntentService;
-import android.content.Context;
 import android.content.Intent;
 import android.content.pm.PackageInfo;
 import android.content.pm.PackageManager;
 import android.content.pm.PackageManager.NameNotFoundException;
-import android.os.AsyncTask;
 import android.os.Environment;
+import android.support.annotation.NonNull;
+import android.text.TextUtils;
 import android.util.Log;
 
+import com.makina.ecrins.commons.BuildConfig;
 import com.makina.ecrins.commons.model.MountPoint;
 import com.makina.ecrins.commons.util.FileUtils;
 
@@ -21,11 +22,11 @@ import java.io.File;
 import java.io.IOException;
 
 /**
- * Executes a list of commands from a given {@link android.content.Intent} :
+ * Executes a list of commands from a given {@code Intent} :
  * <ul>
- * <li>INTENT_PACKAGE_INFO : Gets application information and export as a JSON file (version_<application package name>.json).</li>
- * <li>INTENT_DELETE_INPUT : Deletes a given {@link com.makina.ecrins.commons.input.AbstractInput} file.</li>
- * <li>INTENT_MOVE_FILE_TO_EXTERNAL_STORAGE : Moves a given file to external storage directory used by this context.</li>
+ * <li>{@link #INTENT_PACKAGE_INFO}: Gets application information and export as a JSON file ({@code version_<application package name>.json}).</li>
+ * <li>{@link #INTENT_DELETE_INPUT}: Deletes a given {@link com.makina.ecrins.commons.input.AbstractInput} file.</li>
+ * <li>{@link #INTENT_MOVE_FILE_TO_EXTERNAL_STORAGE}: Moves a given file to external storage directory used by this context.</li>
  * </ul>
  *
  * @author <a href="mailto:sebastien.grimault@makina-corpus.com">S. Grimault</a>
@@ -33,6 +34,8 @@ import java.io.IOException;
 @SuppressLint("Registered")
 public class SyncCommandIntentService
         extends IntentService {
+
+    private static final String TAG = SyncCommandIntentService.class.getName();
 
     /**
      * Command to execute to get application information and export it as a JSON file
@@ -42,12 +45,14 @@ public class SyncCommandIntentService
     /**
      * Command to execute to delete a given {@link com.makina.ecrins.commons.input.AbstractInput} file.
      */
+    @Deprecated
     public static final String INTENT_DELETE_INPUT = "INTENT_DELETE_INPUT";
 
     /**
      * Command to execute to move a given file to external storage directory used by this context
-     * according to {@link com.makina.ecrins.commons.util.FileUtils#getExternalStorageDirectory()}.
+     * according to {@link FileUtils#getExternalStorageDirectory()}.
      */
+    @Deprecated
     public static final String INTENT_MOVE_FILE_TO_EXTERNAL_STORAGE = "INTENT_MOVE_FILE_TO_EXTERNAL_STORAGE";
 
     public static final String INTENT_EXTRA_FILE = "file";
@@ -60,170 +65,126 @@ public class SyncCommandIntentService
     @Override
     protected void onHandleIntent(Intent intent) {
 
-        if (intent.getAction()
-                  .equals(INTENT_PACKAGE_INFO)) {
-            new PackageInfoAsyncTask(this).execute();
-        }
-        else if (intent.getAction()
-                       .equals(INTENT_DELETE_INPUT)) {
-            if (intent.hasExtra(INTENT_EXTRA_FILE)) {
-                new DeleteInputAsyncTask(this).execute(intent.getExtras()
-                                                             .getString(INTENT_EXTRA_FILE));
-            }
-            else {
-                Log.w(getClass().getName(),
-                      "missing key '" + INTENT_EXTRA_FILE + "' for intent '" + INTENT_DELETE_INPUT + "'");
-            }
-        }
-        else if (intent.getAction()
-                       .equals(INTENT_MOVE_FILE_TO_EXTERNAL_STORAGE)) {
-            if (intent.hasExtra(INTENT_EXTRA_FILE)) {
-                new MoveFileToExternalStorageAsyncTask(this).execute(intent.getExtras()
-                                                                           .getString(INTENT_EXTRA_FILE));
-            }
-            else {
-                Log.w(getClass().getName(),
-                      "missing key '" + INTENT_EXTRA_FILE + "' for intent '" + INTENT_MOVE_FILE_TO_EXTERNAL_STORAGE + "'");
-            }
-        }
-        else {
-            Log.w(getClass().getName(),
-                  "no action defined for intent '" + intent.getAction() + "'");
-        }
-    }
+        switch (intent.getAction()) {
+            case INTENT_PACKAGE_INFO:
+                exportPackageInfo();
 
-    private static class PackageInfoAsyncTask
-            extends AsyncTask<Void, Void, Void> {
+                break;
+            case INTENT_DELETE_INPUT:
+                final String inputFilename = intent.getExtras()
+                                                   .getString(INTENT_EXTRA_FILE);
 
-        private Context mContext;
-
-        private PackageInfoAsyncTask(final Context pContext) {
-
-            super();
-
-            this.mContext = pContext;
-        }
-
-        @Override
-        protected Void doInBackground(Void... params) {
-
-            Log.d(getClass().getName(),
-                  "doInBackground");
-
-            PackageManager pm = this.mContext.getPackageManager();
-
-            try {
-                PackageInfo pi = pm.getPackageInfo(this.mContext.getPackageName(),
-                                                   0);
-
-                JSONObject packageInfoJson = new JSONObject();
-                packageInfoJson.put("package",
-                                    this.mContext.getPackageName());
-                packageInfoJson.put("sharedUserId",
-                                    pm.getPackageInfo(this.mContext.getPackageName(),
-                                                      PackageManager.GET_META_DATA).sharedUserId);
-                packageInfoJson.put("versionCode",
-                                    pi.versionCode);
-                packageInfoJson.put("versionName",
-                                    pi.versionName);
-
-                FileUtils.writeStringToFile(FileUtils.getFile(FileUtils.getRootFolder(this.mContext,
-                                                                                      MountPoint.StorageType.INTERNAL),
-                                                              "version_" + this.mContext.getPackageName() + ".json"),
-                                            packageInfoJson.toString());
-            }
-            catch (NameNotFoundException | IOException | JSONException ge) {
-                Log.e(getClass().getName(),
-                      ge.getMessage(),
-                      ge);
-            }
-
-            return null;
-        }
-    }
-
-    private static class DeleteInputAsyncTask
-            extends AsyncTask<String, Void, Void> {
-
-        private Context mContext;
-
-        private DeleteInputAsyncTask(final Context pContext) {
-
-            super();
-
-            this.mContext = pContext;
-        }
-
-        @Override
-        protected Void doInBackground(String... params) {
-
-            try {
-                File inputDir = FileUtils.getInputsFolder(mContext);
-                File inputFile = new File(inputDir,
-                                          params[0]);
-
-                Log.d(getClass().getName(),
-                      "input to delete : " + params[0]);
-
-                // noinspection ResultOfMethodCallIgnored
-                inputFile.delete();
-            }
-            catch (IOException ioe) {
-                Log.e(getClass().getName(),
-                      ioe.getMessage(),
-                      ioe);
-            }
-
-            return null;
-        }
-    }
-
-    private static class MoveFileToExternalStorageAsyncTask
-            extends AsyncTask<String, Void, Void> {
-
-        private Context mContext;
-
-        private MoveFileToExternalStorageAsyncTask(final Context pContext) {
-
-            super();
-
-            this.mContext = pContext;
-        }
-
-        @Override
-        protected Void doInBackground(String... params) {
-
-            try {
-                StringBuilder relativePath = new StringBuilder();
-
-                if (params[0].lastIndexOf("/") != -1) {
-                    relativePath.append(params[0].substring(0,
-                                                            params[0].lastIndexOf("/")));
+                if (TextUtils.isEmpty(inputFilename)) {
+                    Log.w(TAG,
+                          "missing key '" + INTENT_EXTRA_FILE + "' for intent '" + intent.getAction() + "'");
+                }
+                else {
+                    deleteInput(inputFilename);
                 }
 
-                File fileToMove = FileUtils.getFile(Environment.getExternalStorageDirectory(),
-                                                    FileUtils.getRelativeSharedPath(mContext) + params[0]);
+                break;
+            case INTENT_MOVE_FILE_TO_EXTERNAL_STORAGE:
+                final String filename = intent.getExtras()
+                                              .getString(INTENT_EXTRA_FILE);
 
-                if (fileToMove.exists() && !Environment.getExternalStorageDirectory()
-                                                       .getAbsolutePath()
-                                                       .equals(FileUtils.getExternalStorageDirectory()
-                                                                        .getAbsolutePath())) {
-                    FileUtils.deleteQuietly(FileUtils.getFile(FileUtils.getExternalStorageDirectory(),
-                                                              FileUtils.getRelativeSharedPath(mContext) + params[0]));
-                    FileUtils.moveFileToDirectory(fileToMove,
-                                                  FileUtils.getFile(FileUtils.getExternalStorageDirectory(),
-                                                                    FileUtils.getRelativeSharedPath(mContext),
-                                                                    relativePath.toString()),
-                                                  true);
+                if (TextUtils.isEmpty(filename)) {
+                    Log.w(TAG,
+                          "missing key '" + INTENT_EXTRA_FILE + "' for intent '" + intent.getAction() + "'");
                 }
-            }
-            catch (NameNotFoundException | IOException ge) {
-                Log.e(getClass().getName(),
-                      ge.getMessage(),
-                      ge);
+                else {
+                    moveFileToExternalStorage(filename);
+                }
+
+                break;
+            default:
+                Log.w(TAG,
+                      "no action defined for intent '" + intent.getAction() + "'");
+        }
+    }
+
+    private void exportPackageInfo() {
+
+        PackageManager pm = getPackageManager();
+
+        try {
+            PackageInfo pi = pm.getPackageInfo(getPackageName(),
+                                               0);
+
+            JSONObject packageInfoJson = new JSONObject();
+            packageInfoJson.put("package",
+                                getPackageName());
+            packageInfoJson.put("sharedUserId",
+                                pm.getPackageInfo(getPackageName(),
+                                                  PackageManager.GET_META_DATA).sharedUserId);
+            packageInfoJson.put("versionCode",
+                                pi.versionCode);
+            packageInfoJson.put("versionName",
+                                pi.versionName);
+
+            FileUtils.writeStringToFile(FileUtils.getFile(FileUtils.getRootFolder(this,
+                                                                                  MountPoint.StorageType.INTERNAL),
+                                                          "version_" + getPackageName() + ".json"),
+                                        packageInfoJson.toString());
+        }
+        catch (NameNotFoundException | IOException | JSONException ge) {
+            Log.e(TAG,
+                  ge.getMessage(),
+                  ge);
+        }
+    }
+
+    private void deleteInput(@NonNull final String filename) {
+
+        try {
+            File inputDir = FileUtils.getInputsFolder(this);
+            File inputFile = new File(inputDir,
+                                      filename);
+
+            if (BuildConfig.DEBUG) {
+                Log.d(TAG,
+                      "input to delete : " + filename);
             }
 
-            return null;
+            // noinspection ResultOfMethodCallIgnored
+            inputFile.delete();
+        }
+        catch (IOException ioe) {
+            Log.e(TAG,
+                  ioe.getMessage(),
+                  ioe);
+        }
+    }
+
+    private void moveFileToExternalStorage(@NonNull final String filename) {
+
+        try {
+            StringBuilder relativePath = new StringBuilder();
+
+            if (filename.lastIndexOf("/") != -1) {
+                relativePath.append(filename.substring(0,
+                                                       filename.lastIndexOf("/")));
+            }
+
+            File fileToMove = FileUtils.getFile(Environment.getExternalStorageDirectory(),
+                                                FileUtils.getRelativeSharedPath(this) + filename);
+
+            if (fileToMove.exists() && !Environment.getExternalStorageDirectory()
+                                                   .getAbsolutePath()
+                                                   .equals(FileUtils.getExternalStorageDirectory()
+                                                                    .getAbsolutePath())) {
+                FileUtils.deleteQuietly(FileUtils.getFile(FileUtils.getExternalStorageDirectory(),
+                                                          FileUtils.getRelativeSharedPath(this) + filename));
+                FileUtils.moveFileToDirectory(fileToMove,
+                                              FileUtils.getFile(FileUtils.getExternalStorageDirectory(),
+                                                                FileUtils.getRelativeSharedPath(this),
+                                                                relativePath.toString()),
+                                              true);
+            }
+        }
+        catch (NameNotFoundException | IOException ge) {
+            Log.e(TAG,
+                  ge.getMessage(),
+                  ge);
         }
     }
 }
