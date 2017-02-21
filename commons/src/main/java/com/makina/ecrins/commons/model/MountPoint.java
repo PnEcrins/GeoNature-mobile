776ee07844be9463e551c81c307f1125a8763ebb
S. Grimault <sebastien.grimault@gmail.com>
2017-02-21 19:57:03 +0100

    MountPointUtils update: try to resolve all MountPoints from Android APIs only for version >= 19

--- commons/src/main/java/com/makina/ecrins/commons/model/MountPoint.java
+++ commons/src/main/java/com/makina/ecrins/commons/model/MountPoint.java
@@ -1,10 +1,16 @@
 package com.makina.ecrins.commons.model;
 
+import android.os.Environment;
 import android.os.Parcel;
 import android.os.Parcelable;
 import android.support.annotation.NonNull;
+import android.util.Log;
+
+import com.makina.ecrins.commons.BuildConfig;
+import com.makina.ecrins.commons.util.DeviceUtils;
 
 import java.io.File;
+import java.io.IOException;
 
 /**
  * Describes a mount point storage.
@@ -12,7 +18,10 @@ import java.io.File;
  * @author <a href="mailto:sebastien.grimault@makina-corpus.com">S. Grimault</a>
  */
 public class MountPoint
-        implements Parcelable {
+        implements Parcelable,
+                   Comparable<MountPoint> {
+
+    private static final String TAG = MountPoint.class.getName();
 
     @NonNull
     private final File mountPath;
@@ -20,116 +29,110 @@ public class MountPoint
     @NonNull
     private final StorageType storageType;
 
-    public MountPoint(
-            @NonNull final String mountPath,
-            @NonNull final StorageType storageType) {
+    public MountPoint(@NonNull final String mountPath,
+                      @NonNull final StorageType storageType) {
+        String resolvedMountPath;
 
-        this.mountPath = new File(mountPath);
+        try {
+            resolvedMountPath = new File(mountPath).getCanonicalPath();
+
+            if (BuildConfig.DEBUG) {
+                Log.d(TAG,
+                      "MountPoint: '" + mountPath + "', canonical path: '" + resolvedMountPath + "'");
+            }
+        }
+        catch (IOException ioe) {
+            resolvedMountPath = mountPath;
+
+            Log.w(TAG,
+                  "MountPoint: failed to get the canonical path of '" + mountPath + "'");
+        }
+
+        this.mountPath = new File(resolvedMountPath);
         this.storageType = storageType;
     }
 
     private MountPoint(Parcel source) {
-
         this.mountPath = (File) source.readSerializable();
         this.storageType = (StorageType) source.readSerializable();
     }
 
     @NonNull
     public File getMountPath() {
-
         return mountPath;
     }
 
     @NonNull
     public StorageType getStorageType() {
-
         return storageType;
     }
 
-    /**
-     * Returns a boolean indicating whether this {@link MountPoint} is available.
-     *
-     * @return {@code true} if this {@link MountPoint} is available, {@code false} otherwise.
-     *
-     * @see File#exists()
-     */
-    public boolean exists() {
-        return mountPath.exists();
-    }
+    @NonNull
+    public String getStorageState() {
+        if (DeviceUtils.isPostLollipop()) {
+            return Environment.getExternalStorageState(mountPath);
+        }
+        else {
+            String storageState = Environment.MEDIA_UNMOUNTED;
 
-    /**
-     * Indicates whether the current context is allowed to read from this {@link MountPoint}.
-     *
-     * @return {@code true} if this {@link MountPoint} can be read, {@code false} otherwise.
-     *
-     * @see File#canRead()
-     */
-    public boolean canRead() {
-        return mountPath.canRead();
-    }
+            if (mountPath.canWrite()) {
+                storageState = Environment.MEDIA_MOUNTED;
+            }
+            else if (mountPath.canRead()) {
+                storageState = Environment.MEDIA_MOUNTED_READ_ONLY;
+            }
 
-    /**
-     * Indicates whether the current context is allowed to write to this {@link MountPoint}.
-     *
-     * @return {@code true} if this {@link MountPoint} can be written, {@code false} otherwise.
-     *
-     * @see File#canWrite()
-     */
-    public boolean canWrite() {
-        return mountPath.canWrite();
-    }
-
-    /**
-     * Returns the total size in bytes of the partition containing this path.
-     *
-     * @return the total size in bytes of the partition containing this path or 0 if this path does
-     * not exist
-     *
-     * @see File#getTotalSpace()
-     */
-    public long getTotalSpace() {
-
-        return mountPath.getTotalSpace();
-    }
-
-    /**
-     * Returns the number of free bytes on the partition containing this path.
-     *
-     * @return the number of free bytes on the partition containing this path or 0 if this path does
-     * not exist
-     *
-     * @see File#getFreeSpace()
-     */
-    public long getFreeSpace() {
-
-        return mountPath.getFreeSpace();
-    }
-
-    @Override
-    public String toString() {
-
-        return "MountPoint{" +
-                "mountPath=" + mountPath +
-                ", storageType=" + storageType +
-                ", exists=" + mountPath.exists() +
-                ", canRead=" + mountPath.canRead() +
-                ", canWrite=" + mountPath.canWrite() +
-                '}';
+            return storageState;
+        }
     }
 
     @Override
     public int describeContents() {
-
         return 0;
     }
 
     @Override
-    public void writeToParcel(
-            Parcel dest,
-            int flags) {
-
+    public void writeToParcel(Parcel dest,
+                              int flags) {
         dest.writeSerializable(mountPath);
         dest.writeSerializable(storageType);
+    }
+
+    @Override
+    public int compareTo(@NonNull MountPoint mountPoint) {
+        if (storageType.equals(mountPoint.storageType)) {
+            return mountPath.compareTo(mountPoint.mountPath);
+        }
+
+        return storageType.compareTo(mountPoint.storageType);
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) {
+            return true;
+        }
+
+        if (o == null || getClass() != o.getClass()) {
+            return false;
+        }
+
+        MountPoint that = (MountPoint) o;
+
+        return mountPath.equals(that.mountPath);
+    }
+
+    @Override
+    public int hashCode() {
+        return mountPath.hashCode();
+    }
+
+    @Override
+    public String toString() {
+        return "MountPoint{" +
+                "mountPath='" + mountPath + '\'' +
+                ", storageType=" + storageType +
+                '}';
     }
 
     /**
@@ -159,13 +162,11 @@ public class MountPoint
 
         @Override
         public MountPoint createFromParcel(Parcel source) {
-
             return new MountPoint(source);
         }
 
         @Override
         public MountPoint[] newArray(int size) {
-
             return new MountPoint[size];
         }
     };
